050df333fd21a2b2335e6389425aba63917c4d4a
Optimistic Juice <justamanuelax@gmail.com>
2026-02-14 19:25:06 +0200

    Added a useReducer within the Action.tsx also reminded myself within the comments to on how to push my local git to github(remote

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
 
+import Action from "./components/Action";
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Action />
+    </div>
   )
 }
 
-export default App
+export default App;
+// git branch -M main (to switch over to the main branch.)
+// git remote add origin git@github.com:optimisticjuice/giton.git git remote add origin [githuburl]
+// git push -u origin main
+// git push -u origin main
+
+// git branch -M main (to switch over to the main branch.)
+// git remote add origin [githuburl] (git@github.com:[username]/[repository-name].git)
+// git push -u origin main (branch main and push to remote are the same (main))
+
+// git branch -M main (to switch over to the main branch.)
+// git remote add origin [githuburl] (git@github.com:[username]/[repository-name].git)
+// git push -u origin main 
+
+// git branch -M main
+// git remote add origin [githuburl]
+// git push -u origin main
+
+// git clone [githuburl]
+
+// FINAL : 
+// 
+// git branch -M main
+// git remote add origin [githuburl]
+// git push -u origin main
